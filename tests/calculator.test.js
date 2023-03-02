96a99ff0937f5dfe4ed15202fe314d89634bbed4
JERS <ernesto.ramos.sant@gmail.com>
2023-03-02 17:59:03 -0600

    actualizacion

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,25 @@
-const { default: expect } = require('expect');
+const { sum, substract, multiply, divide, power } = require('../calculator');
+
+// const { sum } = require('../calculator');
 
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    //
+    expect(sum(4,2)).toBe(6);//
 })
 
 test('substracts a - b', () => {
-   //
+   expect(substract(4,2)).toBe(2);//
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(multiply(4,2)).toBe(8);//
 })
 
 test('divides a / b', () => {
-    // Consider decimals
+    expect(divide(4,2)).toBe(2);// Consider decimals
 })
 
 test('Raises to power a ^ b', () => {
-    //
-})
-
+    expect(power(4,2)).toBe(16);//
+});
